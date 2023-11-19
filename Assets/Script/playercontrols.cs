4f87c47d30392b72d69b0d757cffbabd1fd725c0
Josh Guinto <134356678+Marcutech0@users.noreply.github.com>
2023-11-20 00:48:17 +0800

    Added Controls

--- Assets/Script/playercontrols.cs
+++ Assets/Script/playercontrols.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class playercontrols : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed;
+    public float jumpForce;
+    private float playerSpeed;
+    public Rigidbody2D rigidBody;
+    private Vector2 movementInput;
+    public Animator anim;
+
+    private bool isGrounded;
+
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        playerSpeed = moveSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        anim.SetFloat("X", movementInput.x);
+        anim.SetFloat("Y", movementInput.y);
+        anim.SetFloat("Speed", movementInput.sqrMagnitude);
+
+        // Check for jump input and if the player is grounded
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Perform grounded check
+        GroundedCheck();
+
+        // Player movement
+        rigidBody.velocity = new Vector2(movementInput.x * playerSpeed, rigidBody.velocity.y);
+    }
+
+    void Jump()
+    {
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+    }
+
+    void GroundedCheck()
+    {
+        // Perform a simple grounded check using a Raycast
+        // Adjust the length of the Raycast depending on your character's size and the ground setup
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
     }
 }
